5eca9b2f76a8718d7d850a79aee856e3ffc0cca6
olujimiAdebakin <omoladebu231@gmail.com>
2025-04-10 10:48:10 +0100

    constants

--- contracts/Constant.sol
+++ contracts/Constant.sol
@@ -8,9 +8,16 @@ pragma solidity ^0.8.18;
 
 contract Constant{
 
-    // 70063
-    address public myAdd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    // USED FRO VARIABLES THAT ARE NOT GONNA CHANGE IN THE CONTRACT
+    // CONSTANT VARIABLE IS USED FOR GAS EFFICIENCY LESSER GAS FEE
 
-    address public constant MY_ADD = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    //	70063
+    // address public myAdd = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
 
+    // 	45697 lesser gas fee
+    address public constant MY_ADDR = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+    function getConstant() public pure returns(address){
+        return MY_ADDR;
+    }
 }
