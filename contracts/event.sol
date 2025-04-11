2705abc7340b986d0ed368da7ff4fcdeca6d9286
olujimiAdebakin <omoladebu231@gmail.com>
2025-04-11 19:42:31 +0100

    event

--- contracts/event.sol
+++ contracts/event.sol
@@ -2,4 +2,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+contract Event {
+    // event declaration
+    // up to 3 parameters can be idexed.
+    // Indexed parameters helps you filter the logs by the indexed parameter
 
+    event Log (address indexed sender, string message);
+    event AnotherLog();
+
+    function test() public {
+        emit Log(msg.sender, "Hello world");
+        emit Log(msg.sender, "Hello Buterin");
+        emit Log(msg.sender, "Hello Olujimi");
+        emit AnotherLog();
+    }
+
+    
+}
